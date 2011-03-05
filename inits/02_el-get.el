2727bd12251b046496821372422b69bb4b8b491b
taks <857tn859@gmail.com>
2011-03-05 15:41:13 +0900

    el-get がインストールされていない場合に，エラーを表示するようにした

--- inits/02_el-get.el
+++ inits/02_el-get.el
@@ -1,7 +1,8 @@
 ;;; init-el-get.el ---
 
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-(require 'el-get)
+(when (not (require 'el-get nil t))
+  (error "Please bootstrap el-get using the instructions here: http://github.com/dimitri/el-get/, then restart Emacs"))
 
 (setq el-get-sources
       '(el-get
