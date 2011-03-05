5b04aca76d5cc7e5a4492146d8600676df97235b
taks <857tn859@gmail.com>
2011-03-05 15:39:50 +0900

    elpa がインストールされていない場合に，エラーを表示するようにした

--- inits/01_elpa.el
+++ inits/01_elpa.el
@@ -6,10 +6,10 @@
 ;;; interfacing with ELPA, the package archive.
 ;;; Move this code earlier if you want to reference
 ;;; packages in your .emacs.
-(when
-    (load
-     (expand-file-name "~/.emacs.d/elpa/package.el"))
-  (package-initialize))
+(when (not (load (expand-file-name "~/.emacs.d/elpa/package.el") t))
+  (error "Please bootstrap elpa using the instructions here: http://tromey.com/elpa/install.html, then restart Emacs"))
+
+(package-initialize)
 (setq package-archives '(("ELPA" . "http://tromey.com/elpa/")
                          ("gnu"  . "http://elpa.gnu.org/packages/")
                          ("SC"   . "http://joseito.republika.pl/sunrise-commander/")))
