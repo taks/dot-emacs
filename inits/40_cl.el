1b88a0f70208da94e9e282340261b7d14ec22f6d
taks <857tn859@gmail.com>
2011-04-08 00:28:46 +0900

    slime に関連した pop-win の設定を追加

--- inits/40_cl.el
+++ inits/40_cl.el
@@ -9,3 +9,13 @@
           (lambda ()
             (require 'ac-slime)
             (set-up-slime-ac)))
+
+;; pop-win の設定
+(push '("*slime-apropos*") popwin:special-display-config)
+(push '("*slime-macroexpansion*") popwin:special-display-config)
+(push '("*slime-description*") popwin:special-display-config)
+(push '("*slime-compilation*" :noselect t) popwin:special-display-config)
+(push '("*slime-xref*") popwin:special-display-config)
+(push '(sldb-mode :stick t) popwin:special-display-config)
+(push '(slime-repl-mode) popwin:special-display-config)
+(push '(slime-connection-list-mode) popwin:special-display-config)
