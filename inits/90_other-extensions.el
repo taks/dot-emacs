065541e272bb896fd7e31280dde3bd48faf86835
taks <857tn859@gmail.com>
2011-09-20 15:10:46 +0900

    windows の設定

--- inits/90_other-extensions.el
+++ inits/90_other-extensions.el
@@ -19,3 +19,18 @@
   )
 
 (require 'anything-coding-system)
+
+
+(progn
+  ;; windows
+  ;; @see: http://d.hatena.ne.jp/khiker/20110508/windowsel
+  (setq win:use-frame nil
+        win:quick-selection nil
+        ;; use a-z as prefix.
+        win:base-key ?`
+        win:switch-prefix "\C-z"
+        win:max-configs 27)
+  (require 'windows)
+  (win:startup-with-window)
+  (define-key win:switch-map ";" 'win-switch-menu)
+  (define-key ctl-x-map "C" 'see-you-again))
