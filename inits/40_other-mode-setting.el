1127cb6aa936d0ad1311dd061a16ea6bb5741523
taks <857tn859@gmail.com>
2012-01-06 19:07:09 +0900

    diff-mode の見た目を設定

--- inits/40_other-mode-setting.el
+++ inits/40_other-mode-setting.el
@@ -113,3 +113,17 @@
 ;; scss-mode
 (setq scss-compile-at-save nil) ;; 自動コンパイルをオフにする
 (setq css-indent-offset 2) ;; インデント量
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; diff-mode
+(set-face-attribute 'diff-added-face nil
+                    :background nil :foreground "green"
+                    :weight 'normal)
+(set-face-attribute 'diff-removed-face nil
+                    :background nil :foreground "firebrick1"
+                    :weight 'normal)
+(set-face-attribute 'diff-file-header-face nil
+                    :background nil :weight 'extra-bold)
+(set-face-attribute 'diff-hunk-header-face nil
+                    :foreground "chocolate4"
+                    :background "white" :weight 'extra-bold
+                    :underline t :inherit nil)
