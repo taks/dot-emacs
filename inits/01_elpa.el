391ef0be899d8c494793f3dd969178df6328b11a
taks <857tn859@gmail.com>
2013-01-04 11:50:27 +0900

    elpa の設定を変更

--- inits/01_elpa.el
+++ inits/01_elpa.el
@@ -1,15 +1,7 @@
 ;;; 01_elpa.el ---
 ;; @see: http://tromey.com/elpa/
 
-;;; This was installed by package-install.el.
-;;; This provides support for the package system and
-;;; interfacing with ELPA, the package archive.
-;;; Move this code earlier if you want to reference
-;;; packages in your .emacs.
-(when (not (load (expand-file-name "~/.emacs.d/elpa/package.el") t))
-  (error "Please bootstrap elpa using the instructions here: http://tromey.com/elpa/install.html, then restart Emacs"))
-
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
-(setq package-archives '(("ELPA" . "http://tromey.com/elpa/")
-                         ("gnu"  . "http://elpa.gnu.org/packages/")
-                         ("SC"   . "http://joseito.republika.pl/sunrise-commander/")))
