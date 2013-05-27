5a6c76939f2b0417ac696b3e4f6e0ea8067bb4d9
taks <857tn859@gmail.com>
2013-05-27 23:48:22 +0900

    update ghc-mod version

--- inits/40_haskell.el
+++ inits/40_haskell.el
@@ -3,7 +3,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ghc-mod
 (add-path (expand-file-name "~/.cabal/bin"))
-(add-to-list 'load-path "~/.cabal/share/ghc-mod-0.6.0")
+(add-to-list 'load-path "~/.cabal/share/ghc-mod-2.0.2")
 (autoload 'ghc-init "ghc" nil t)
 (add-hook 'haskell-mode-hook (lambda () (ghc-init) (flymake-mode)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
