71d58c866aba8292e50632c6fce4f5ae0916d7bd
taks <857tn859@gmail.com>
2014-11-27 23:49:34 +0900

    delete my-helm-toggle-resplit-window function

--- inits/45_helm.el
+++ inits/45_helm.el
@@ -16,22 +16,4 @@
   (setq popwin:special-display-config '(("*compilatoin*" :noselect t)
                                         ("helm" :regexp t :height 0.5))))
 
-(progn
-  ;; 縦分割/横分割切り替え
-  ;; @see: http://d.hatena.ne.jp/grandVin/20090716/1247725452
-  (defun my-helm-toggle-resplit-window ()
-    (interactive)
-    (when (helm-window)
-      (save-selected-window
-        (select-window (helm-window))
-        (let ((before-height (window-height)))
-          (delete-other-windows)
-          (switch-to-buffer helm-current-buffer)
-          (if (= (window-height) before-height)
-              (split-window-vertically)
-            (split-window-horizontally)))
-        (select-window (next-window))
-        (switch-to-buffer helm-buffer))))
-  (define-key helm-map (kbd "C-o") 'my-helm-toggle-resplit-window))
-
 (require 'helm-coding-system)
