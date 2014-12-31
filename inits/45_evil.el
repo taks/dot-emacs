ef63806fc5a957ba35b3723e20e6ae61ea8eb4b7
taks <857tn859@gmail.com>
2014-12-31 22:29:56 +0900

    modify evil key map

--- inits/45_evil.el
+++ inits/45_evil.el
@@ -13,8 +13,15 @@
   (evil-write nil nil nil file bang)
   (evil-delete-buffer (current-buffer)))
 (evil-ex-define-cmd "wq" 'evil-save-and-delete-buffer)
+(evil-ex-define-cmd "q[uit]" 'kill-this-buffer)
 
-;; keybind
+;;; keybind
 (define-key evil-insert-state-map "\C-k" nil)
 
+;; 物理行で移動
+(define-key evil-motion-state-map "j" 'evil-next-visual-line)
+(define-key evil-motion-state-map "k" 'evil-previous-visual-line)
+
 (define-key evil-motion-state-map "/" 'helm-swoop)
+
+(define-key evil-normal-state-map "f" 'jaunte)
